7e9aabba357926769d9638d2760140b36f796b01
Adrian Herridge <editfmah@yahoo.co.uk>
2018-08-02 12:52:33 +0100

    updated package versions

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("0.8.3")),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .exact("3.0.10")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("0.11.0")),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .exact("3.0.17")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", .exact("3.1.1")),
         .package(url: "https://gitlab.com/katalysis/Ed25519.git", .exact("0.2.1")),
         .package(url: "https://github.com/sharksync/SWSQLite.git", .exact("1.0.11")),
