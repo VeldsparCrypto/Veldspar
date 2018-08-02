04d23d99cd937c9096176c49a7e2eaddf64b5fae
Adrian Herridge <editfmah@yahoo.co.uk>
2018-08-02 14:18:58 +0100

    fixed shitty PM issue

--- Package.swift
+++ Package.swift
@@ -14,28 +14,27 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("0.11.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("0.8.3")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .exact("3.0.17")),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Session.git", .exact("3.1.1")),
         .package(url: "https://gitlab.com/katalysis/Ed25519.git", .exact("0.2.1")),
         .package(url: "https://github.com/sharksync/SWSQLite.git", .exact("1.0.11")),
         ],
     targets: [
         .target(
             name: "veldspard",
-            dependencies: ["CryptoSwift","PerfectHTTPServer","PerfectSession","SWSQLite","VeldsparCore","Ed25519"],
+            dependencies: ["CryptoSwift","PerfectHTTPServer","SWSQLite","VeldsparCore","Ed25519"],
             path: "./Sources/daemon"),
         .target(
             name: "miner",
-            dependencies: ["CryptoSwift","PerfectHTTPServer","PerfectSession","SWSQLite","VeldsparCore","Ed25519"],
+            dependencies: ["CryptoSwift","PerfectHTTPServer","SWSQLite","VeldsparCore","Ed25519"],
             path: "./Sources/miner"),
         .target(
             name: "simplewallet",
-            dependencies: ["CryptoSwift","PerfectHTTPServer","PerfectSession","SWSQLite","VeldsparCore","Ed25519"],
+            dependencies: ["CryptoSwift","PerfectHTTPServer","SWSQLite","VeldsparCore","Ed25519"],
             path: "./Sources/simplewallet"),
         .target(
             name: "VeldsparCore",
-            dependencies: ["CryptoSwift","PerfectHTTPServer","PerfectSession","SWSQLite","Ed25519"],
+            dependencies: ["CryptoSwift","PerfectHTTPServer","SWSQLite","Ed25519"],
             path: "./Sources/core"),
         ]
 )
