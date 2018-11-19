317d785762dfc8511ce97b4442c303ddcd542e61
Adrian Herridge <editfmah@yahoo.co.uk>
2018-11-19 22:14:24 +0000

    crazy swift compile issue

--- Sources/core/utilities.swift
+++ Sources/core/utilities.swift
@@ -336,7 +336,7 @@ public extension MutableCollection {
         
         for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
             // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
-            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let d: Int = Int(arc4random_uniform(UInt32(unshuffledCount)))
             let i = index(firstUnshuffled, offsetBy: d)
             swapAt(firstUnshuffled, i)
         }
